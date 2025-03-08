75ba6493fc218911d8a9f3579e44d0081fcaec5e
g7-bit <65290200+g7-bit@users.noreply.github.com>
2025-03-08 13:02:14 +0530

    added index route and views folder

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express= require("express");
 const app = express();
 const mongoose = require("mongoose")
-// const ejs = requrie("ejs")
+const Listing = require("./models/listing.js")
+const path = require("path")
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/test";
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname,"views"))
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 async function main() {
     await mongoose.connect(MONGO_URL);
     
@@ -20,6 +24,31 @@ main()
 app.get("/", (req,res)=>{
     res.send("working")
 })
+
+//INDEX route
+app.get("/listings", async(req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings})
+
+})
+
+
+
+
+// app.get("/testlisting", async(req,res)=>{
+//     let sampleListing = new Listing({
+//         title: "My New Villa ",
+//         description: "By the  beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India",
+//     })
+
+//     await sampleListing.save()
+//     console.log("sample was saved")
+
+//     res.send("successfull Testing")
+// })
 
 app.listen(8080, ()=>{
     console.log("server is listeneing on port 8080")
